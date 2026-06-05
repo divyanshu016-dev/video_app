de6036a0083a266e83efc370b0dee03fd7366a48
divyanshu016-dev <divyanshu235sharma@gmail.com>
2026-06-05 10:17:01 +0530

    SO I MADE FEW CHANGES IN MY VERY FIRST CODE HOPE TO DO MY BEST IN MY UPCOMING COMMITS

---
+++ build/CMakeFiles/video_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video_app.dir/src/main.cpp.obj"
+  "CMakeFiles/video_app.dir/src/main.cpp.obj.d"
+  "libvideo_app.dll.a"
+  "video_app.exe"
+  "video_app.exe.manifest"
+  "video_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
